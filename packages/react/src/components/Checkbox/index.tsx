25801888f54d7966454809ed1fb407735eb71bb0
daniel.carvalho <daniel.carvalho@maxmilhas.com.br>
2022-11-23 09:04:22 -0300

    Add checkbox component

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,18 @@
+import { Check } from 'phosphor-react'
+import { ComponentProps } from 'react'
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxContainer> {}
+
+// asChild => Elemento não será recriado em tela,
+// apenas repassa suas propriedades para seu filho
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
